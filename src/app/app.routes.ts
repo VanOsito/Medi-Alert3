0ad22db3eb8593d7ab44dba33b3c7c5b1a7262c1
VanOsito <van.troncosom@duocuc.cl>
2024-05-29 16:53:59 -0400

    woooooooooooow 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,24 +14,4 @@ export const routes: Routes = [
     path: 'inicio',
     loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
   },
-  {
-    path: 'pregunta',
-    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
-  },
-  {
-    path: 'registrar',
-    loadComponent: () => import('./pages/registrar/registrar.page').then( m => m.RegistrarPage)
-  },
-  {
-    path: 'correo',
-    loadComponent: () => import('./pages/correo/correo.page').then( m => m.CorreoPage)
-  },
-  {
-    path: 'correcto',
-    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
-  },
-  {
-    path: 'incorrecto',
-    loadComponent: () => import('./pages/incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
-  },
 ];
